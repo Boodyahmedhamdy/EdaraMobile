8806b1697baf848318b701c5bc6fbf01a329d86a
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2024-08-25 13:00:37 +0300

    created component to be used for input strings

---
+++ app/src/main/java/com/example/edaramobile/authentication/ui/components/TextEntryTextField.kt
@@ -0,0 +1,46 @@
+package com.example.edaramobile.authentication.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun TextEntryTextField(
+    modifier: Modifier = Modifier,
+    labelString: String = "",
+    value: String = "",
+    onValueChanged: (String) -> Unit = {},
+    leadingIcon: @Composable (() -> Unit)? = null,
+    singleLine: Boolean = true,
+    suffixText: String = ""
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Text(
+            labelString,
+            color = MaterialTheme.colorScheme.primary,
+            fontSize = 24.sp,
+            textAlign = TextAlign.Start,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        OutlinedTextField(
+            value = value,
+            onValueChange = { onValueChanged(it) },
+            leadingIcon = leadingIcon,
+            label = { Text(labelString) },
+            singleLine = singleLine,
+            suffix = { Text(suffixText) },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+    }
+
+}
