16bb13a8aa82e88668ebb47ade07e5e4e14f499c
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2024-08-26 10:00:43 +0300

    used placeholder instead of empty body

--- app/src/main/java/com/example/edaramobile/authentication/ui/screens/RegisterScreen.kt
+++ app/src/main/java/com/example/edaramobile/authentication/ui/screens/RegisterScreen.kt
@@ -1,5 +1,6 @@
 package com.example.edaramobile.authentication.ui.screens
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -10,14 +11,16 @@ fun RegisterScreen(
     state: RegisterScreenUiState,
     modifier: Modifier = Modifier
 ) {
+    PlaceHolderScreen(title = "Register Screen", modifier = modifier)
 
 }
 
 
-@Preview
+@Preview(showSystemUi = true, showBackground = true)
 @Composable
 private fun RegisterScreenPreview() {
     RegisterScreen(
-        RegisterScreenUiState()
+        RegisterScreenUiState(),
+        modifier = Modifier.fillMaxSize()
     )
 }
