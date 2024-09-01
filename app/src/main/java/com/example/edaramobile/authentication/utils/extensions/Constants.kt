3f6d62b809ac2cad5871aaf64fcc096c32c2152a
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2024-09-01 11:03:59 +0300

    changed Edara to edara with lowercase e

--- app/src/main/java/com/example/edaramobile/authentication/utils/extensions/Constants.kt
+++ app/src/main/java/com/example/edaramobile/authentication/utils/extensions/Constants.kt
@@ -1,6 +1,6 @@
 package com.example.edaramobile.authentication.utils.extensions
 
 object Constants {
-    const val SUFFIX_USERNAME: String = "@Edara.com"
+    const val SUFFIX_USERNAME: String = "@edara.com"
 }
 
