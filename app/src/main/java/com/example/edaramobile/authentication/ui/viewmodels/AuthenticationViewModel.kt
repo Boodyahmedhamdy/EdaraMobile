2f27ac5a8b95ca334b2479fc3aa579997ff60e97
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2024-09-02 10:05:41 +0300

    added register screen validation logic for names

--- app/src/main/java/com/example/edaramobile/authentication/ui/viewmodels/AuthenticationViewModel.kt
+++ app/src/main/java/com/example/edaramobile/authentication/ui/viewmodels/AuthenticationViewModel.kt
@@ -1,13 +1,18 @@
 package com.example.edaramobile.authentication.ui.viewmodels
 
 import androidx.lifecycle.ViewModel
+import com.example.edaramobile.authentication.domain.models.ValidationResult
+import com.example.edaramobile.authentication.domain.usecases.validation.ValidateFirstNameUseCase
+import com.example.edaramobile.authentication.domain.usecases.validation.ValidateLastNameUseCase
 import com.example.edaramobile.authentication.ui.states.LoginScreenUiState
 import com.example.edaramobile.authentication.ui.states.RegisterScreenUiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
-class AuthenticationViewModel: ViewModel() {
+class AuthenticationViewModel(
+//    private val validateFirstNameUseCase: ValidateFirstNameUseCase
+): ViewModel() {
 
 
     // -------------- Login Screen START --------------------
@@ -49,20 +54,29 @@ class AuthenticationViewModel: ViewModel() {
     fun updateFirstNameInRegisterScreen(firstName: String) {
         _registerScreenState.update { it.copy(firstName = firstName) }
     }
+    fun updateFirstNameErrorMessageInRegisterScreen(firstNameErrorMessage: String) {
+        _registerScreenState.update { it.copy(firstNameErrorMessage = firstNameErrorMessage) }
+    }
     fun updateLastNameInRegisterScreen(lastName: String) {
         _registerScreenState.update { it.copy(lastName = lastName) }
+    }
+    fun updateLastNameErrorMessageInRegisterScreen(lastNameErrorMessage: String) {
+        _registerScreenState.update { it.copy(lastNameErrorMessage = lastNameErrorMessage) }
     }
     fun updateUsernameInRegisterScreen(username: String) {
         _registerScreenState.update { it.copy(username = username) }
     }
+    fun updateUsernameErrorMessageInRegisterScreen(usernameErrorMessage: String) {
+        _registerScreenState.update { it.copy(usernameErrorMessage = usernameErrorMessage) }
+    }
     fun updatePasswordInRegisterScreen(password: String) {
         _registerScreenState.update { it.copy(password = password) }
     }
-    fun updateConfirmedPasswordInRegisterScreen(confirmedPassword: String) {
-        _registerScreenState.update { it.copy(confirmedPassword = confirmedPassword) }
-    }
     private fun updatePasswordErrorMessageInRegisterScreen(passwordErrorMessage: String) {
         _registerScreenState.update { it.copy(passwordErrorMessage = passwordErrorMessage) }
+    }
+    fun updateConfirmedPasswordInRegisterScreen(confirmedPassword: String) {
+        _registerScreenState.update { it.copy(confirmedPassword = confirmedPassword) }
     }
     private fun updateConfirmedPasswordErrorMessageInRegisterScreen(confirmedPasswordErrorMessage: String) {
         _registerScreenState.update { it.copy(confirmedPasswordErrorMessage = confirmedPasswordErrorMessage) }
@@ -72,10 +86,51 @@ class AuthenticationViewModel: ViewModel() {
     }
 
     fun register() {
-
+        if(isValidRegisterScreenData()) {
+            // register logic
+        }
     }
 
 
     // -------------- Register Screen END ------------------------
+
+    // ----------------- Common Logic Start ------------------------
+    private fun validateFirstName(firstName: String): Boolean {
+//        val result = validateFirstNameUseCase(firstName)
+        val result = ValidateFirstNameUseCase().invoke(firstName)
+        return when(result) {
+            is ValidationResult.FailureValidationResult -> {
+                updateFirstNameErrorMessageInRegisterScreen(result.errorMessage)
+                false
+            }
+            ValidationResult.SuccessValidationResult -> {
+                updateFirstNameErrorMessageInRegisterScreen("")
+                true
+            }
+        }
+    }
+
+    private fun validateLastName(lastName: String): Boolean {
+        val result = ValidateLastNameUseCase().invoke(lastName)
+        return when(result) {
+            is ValidationResult.FailureValidationResult -> {
+                updateLastNameErrorMessageInRegisterScreen(result.errorMessage)
+                false
+            }
+            ValidationResult.SuccessValidationResult -> {
+                updateLastNameErrorMessageInRegisterScreen("")
+                true
+            }
+        }
+    }
+
+    private fun isValidRegisterScreenData(): Boolean {
+        return validateFirstName(registerScreenState.value.firstName)
+                && validateLastName(registerScreenState.value.lastName)
+
+    }
+
+
+    // ----------------- Common Logic END ------------------------
 
 }
