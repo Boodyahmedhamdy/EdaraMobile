6c121754841481280a1518adda476ec5a50e23e8
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2024-09-02 09:55:18 +0300

    added constants file for validation process .. useful in error message

---
+++ app/src/main/java/com/example/edaramobile/authentication/domain/usecases/validation/ValidationConstants.kt
@@ -0,0 +1,10 @@
+package com.example.edaramobile.authentication.domain.usecases.validation
+
+object ValidationConstants {
+    const val MIN_FIRST_NAME_LENGTH = 3
+    const val MAX_FIRST_NAME_LENGTH = 50
+
+
+    const val MIN_LAST_NAME_LENGTH = 3
+    const val MAX_LAST_NAME_LENGTH = 50
+}
