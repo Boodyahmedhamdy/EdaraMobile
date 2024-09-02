094786fe19a423b6a14c0e9f787567e4f762adba
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2024-09-02 09:58:36 +0300

    added error messages for remaining feilds

--- app/src/main/java/com/example/edaramobile/authentication/ui/states/RegisterScreenUiState.kt
+++ app/src/main/java/com/example/edaramobile/authentication/ui/states/RegisterScreenUiState.kt
@@ -2,8 +2,11 @@ package com.example.edaramobile.authentication.ui.states
 
 data class RegisterScreenUiState(
     val username: String = "",
+    val usernameErrorMessage: String = "",
     val firstName: String = "",
+    val firstNameErrorMessage: String = "",
     val lastName: String = "",
+    val lastNameErrorMessage: String = "",
     val password: String = "",
     val passwordErrorMessage: String = "",
     val confirmedPassword: String = "",
